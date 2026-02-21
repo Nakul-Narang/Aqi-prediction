db3ec3bac2e73e0e340858b4f571491178d14f13
Nakul-Narang <nakulnarang2005@gmail.com>
2026-02-21 13:49:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,31 +17,26 @@ pollution_type = st.selectbox("Pollution Type", pollution_encoder.classes_)
 year = st.number_input("Year", min_value=2000, max_value=2100, value=2024)
 month = st.number_input("Month", min_value=1, max_value=12, value=1)
 day = st.number_input("Day", min_value=1, max_value=31, value=1)
-
 if st.button("Predict"):
 
-    # Encode values
     # Encode inputs
-      city_enc = city_encoder.transform([city])[0]
-      state_enc = state_encoder.transform([state])[0]
-      pollutant_enc = pollution_level_encoder.transform([pollution_type])[0]
+    city_enc = city_encoder.transform([city])[0]
+    state_enc = state_encoder.transform([state])[0]
+    pollutant_enc = pollution_encoder.transform([pollution_type])[0]
 
-# Create dataframe EXACTLY like model expects
-      input_df = pd.DataFrame({
+    # Create dataframe EXACTLY like model expects
+    input_df = pd.DataFrame({
         'city_enc': [city_enc],
         'state_enc': [state_enc],
         'pollutant_enc': [pollutant_enc],
         'year': [year],
         'month': [month]
-})
+    })
 
-# Prediction
-
-
-    # Create DataFrame EXACTLY matching training
     # Ensure correct column order
-       input_df = input_df[['city_enc', 'state_enc', 'pollutant_enc', 'year', 'month', 'day']]
+    input_df = input_df[['city_enc', 'state_enc', 'pollutant_enc', 'year', 'month']]
 
+    # Predict
     prediction = model.predict(input_df)
 
     st.success(f"Predicted AQI Value: {prediction[0]}")
